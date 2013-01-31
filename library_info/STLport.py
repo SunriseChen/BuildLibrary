b1e761fbea4ef3d8ea7f685cf8fb0420deed34f6
Sunrise <sunrise0918@gmail.com>
2013-01-31 17:53:39 +0800

    添加 STLport 的初步设定

---
+++ library_info/STLport.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+dist = setup(
+		name='STLport',
+		version='5.2.1',
+		#scripts=['.py'],
+
+		author='Petr Ovtchenkov',
+		author_email='',
+		description='Multiplatform C++ Standard Library (STL implementation). Many compilers and operational environments supported. Standard (ISO/IEC 14882) compliance. Maximum efficiency. Exception and thread safety. Debug mode.',
+		license='Other License',
+		keywords='',
+		url='http://stlport.org',
+
+		download_url='http://sourceforge.net/projects/stlport/files/latest/download',
+		)
+
