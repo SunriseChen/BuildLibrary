df91280164ee856d531cf74a78b2105d6343a9ee
Sunrise <sunrise0918@gmail.com>
2013-01-31 12:32:34 +0800

    暂时去掉验证文件的处理

--- bl_setup.py
+++ bl_setup.py
@@ -6,7 +6,7 @@ import os, sys, shutil
 
 DEFAULT_VERSION = '0.1.0'
 DEFAULT_URL     = 'https://github.com/SunriseChen/BuildLibrary/archive/master.zip'
-MD5_DIGEST = 'b59cb88bd745078bcee4568b9162aaca'
+FILE_DIGEST = ''
 PACK_FILE_ROOT_DIR = 'BuildLibrary-master'
 
 
@@ -35,13 +35,30 @@ def validate_md5(data, digest):
 		# Python version < 2.5
 		from md5 import md5
 
-	#print(md5(data).hexdigest())
 	return (digest == md5(data).hexdigest())
+
+
+def validate_sha1(data, digest):
+	sha1 = None
+	try:
+		# Python version >= 2.5
+		import hashlib
+		sha1 = hashlib.sha1(data)
+	except ImportError:
+		# Python version < 2.5
+		import sha
+		sha1 = sha.new(data)
+
+	return (digest == sha1.hexdigest())
 
 
 def validate_file(file_name, file_digest):
 	with open(file_name, 'rb') as file_data:
-		return validate_md5(file_data.read(), file_digest)
+		digest_len = len(file_digest)
+		if digest_len == 32:
+			return validate_md5(file_data.read(), file_digest)
+		elif digest_len == 40:
+			return validate_sha1(file_data.read(), file_digest)
 
 	return False
 
@@ -95,7 +112,8 @@ def move_files(src_dir, dst_dir, ignore=None):
 
 
 def build_lib_setup():
-	file_name = download_and_validate(DEFAULT_URL, MD5_DIGEST)
+	#file_name = download_and_validate(DEFAULT_URL, FILE_DIGEST)
+	file_name = download_file(DEFAULT_URL)
 	if file_name is None:
 		sys.exit(1)
 	unpack_dir = unpack_file(file_name)
