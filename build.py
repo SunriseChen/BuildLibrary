9b8ba7277cebc657be166e0b6866fb313aef3ed2
Sunrise <sunrise0918@gmail.com>
2013-02-04 17:43:07 +0800

    初步流程

--- build.py
+++ build.py
@@ -1,47 +1,55 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os, sys, shutil
-from distutils.dist import Distribution
-from distutils.core import run_setup
+import os
 
 
-def make_lib_info():
-	libs = []
+def get_lib_info():
+	lib_info = {}
 	for root, dirs, files in os.walk('lib_info'):
 		for f in files:
 			lib_name, ext = os.path.splitext(f)
 			if ext.lower() == '.py' and not lib_name.lower().endswith('__init__'):
+				lib_name = os.path.join(root[9:], lib_name).lower()
+				lib_name = lib_name.replace('\\', '.')
+				lib_name = lib_name.replace('/', '.')
 				lib_info_path = os.path.join(root, f)
-				libs.append(lib_info_path)
+				lib_info[lib_name] = lib_info_path
 	
-	return libs
+	return lib_info
 
 
-class LibraryDistribution(Distribution):
-	pass
-	#lib_dist = run_setup(lib_info_path, stop_after='commandline')
-
-	#find()
-	#compare()
-	#download()
-	#unpack()
-	#build()
-	#install()
-	#clean()
+def install(lib_name, lib_version=None):
+	lib_dist = __import__(lib_name)
+	download_url = lib_dist.find(lib_version)
+	if download_url:
+		pack_file = lib_dist.download(download_url)
+		unpack_dir = lib_dist.unpack(pack_file)
+		lib_dist.build(unpack_dir)
+		lib_dist.install(unpack_dir)
+		lib_dist.clean()
+	else:
+		print('Current version is your need.')
 
 
 def main():
 	import argparse
+	import subprocess
+
+	lib_info = get_lib_info()
 
 	parser = argparse.ArgumentParser(description='build lib')
-	parser.add_argument('lib_name', metavar='lib', nargs='+',
+	parser.add_argument('lib', metavar='lib', nargs='+',
 			help='Library name')
 
 	args = parser.parse_args()
-	print(args.lib_name)
+	for lib in args.lib:
+		lib_name = lib.lower()
+		if lib_name in lib_info:
+			subprocess.call(['python', lib_info[lib_name], 'install'])
+		else:
+			print(lib + ' is not supported.')
 
 
 if __name__ == '__main__':
 	main()
-	print make_lib_info()
--- lib_info/STLport.py
+++ lib_info/STLport.py
@@ -2,6 +2,29 @@
 # -*- coding: utf-8 -*-
 
 from distutils.core import setup
+from distutils.dist import Distribution
+
+
+class LibraryDistribution(Distribution):
+
+	#def parse_config_files(self):
+	#	Distribution.parse_config_files(self)
+
+	#def parse_command_line(self):
+	#	return Distribution.parse_command_line(self)
+
+	def get_command_class(self, command):
+		klass = Distribution.get_command_class(self, command)
+		#print('klass = ')
+		#print(klass)
+		return klass
+
+	def get_command_obj(self, command, create=1):
+		cmd_obj = Distribution.get_command_obj(self, command, create)
+		#print('cmd_obj =')
+		#print(cmd_obj)
+		return cmd_obj
+
 
 setup(
 		name='STLport',
@@ -16,5 +39,6 @@ setup(
 		url='http://stlport.org',
 
 		download_url='http://sourceforge.net/projects/stlport/files/latest/download',
+		distclass=LibraryDistribution,
 		)
 
