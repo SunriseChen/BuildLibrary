72b38bf4df6d84b2a15d6b734390a51cebaecb63
Sunrise <sunrise0918@gmail.com>
2013-02-05 18:03:13 +0800

    添加版本比较

--- build.py
+++ build.py
@@ -1,40 +1,78 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
+import os, sys
+from pkg_resources import parse_version
+
+LIB_INFO_DIR = 'lib_info'
 
 
 def get_lib_info():
 	lib_info = {}
-	for root, dirs, files in os.walk('lib_info'):
+	for root, dirs, files in os.walk(LIB_INFO_DIR):
 		for f in files:
 			lib_name, ext = os.path.splitext(f)
 			if ext.lower() == '.py' and not lib_name.lower().endswith('__init__'):
-				lib_name = os.path.join(root[9:], lib_name).lower()
+				lib_name = os.path.join(root, lib_name)
 				lib_name = lib_name.replace('\\', '.')
 				lib_name = lib_name.replace('/', '.')
-				lib_info_path = os.path.join(root, f)
-				lib_info[lib_name] = lib_info_path
+				# from lib_name import get_dist_list
+				lib = __import__(lib_name, globals(), locals(), ['get_dist_list'])
+				if hasattr(lib, 'get_dist_list'):
+					lib_info[lib_name[9:].lower()] = lib.get_dist_list()
 	
 	return lib_info
 
 
-def install(lib_name, lib_version=None):
-	lib_dist = __import__(lib_name)
-	download_url = lib_dist.find(lib_version)
-	if download_url:
+def get_local_version(dist_list):
+	return parse_version('5.2.1')
+
+
+def get_lib_dist(dist_list, version=None, cmp=None):
+	#sorted(dist_list)
+
+	if version is None:
+		version = dist_list[0].get_version()
+	if cmp is None:
+		cmp = '>='
+
+	version = parse_version(version)
+	local_version = get_local_version(dist_list)
+	if cmp == '==' and local_version == version or	\
+		cmp == '!=' and local_version != version or	\
+		cmp == '>' and local_version > version or	\
+		cmp == '>=' and local_version >= version or	\
+		cmp == '<' and local_version < version or	\
+		cmp == '<=' and local_version <= version:
+		print('Current version is your need.')
+		return None
+
+	for dist in dist_list:
+		remote_version = parse_version(dist.get_version())
+		if cmp == '==' and remote_version == version or		\
+			cmp == '!=' and remote_version != version or	\
+			cmp == '>' and remote_version > version or		\
+			cmp == '>=' and remote_version >= version or	\
+			cmp == '<' and remote_version < version or		\
+			cmp == '<=' and remote_version <= version:
+			return dist
+
+	print('Did not comply with the version.')
+	return None
+
+
+def install(dist_list, version=None, cmp=None):
+	lib_dist = get_lib_dist(dist_list, version, cmp)
+	if lib_dist:
 		pack_file = lib_dist.download(download_url)
 		unpack_dir = lib_dist.unpack(pack_file)
 		lib_dist.build(unpack_dir)
 		lib_dist.install(unpack_dir)
 		lib_dist.clean()
-	else:
-		print('Current version is your need.')
 
 
 def main():
-	import argparse
-	import subprocess
+	import argparse, subprocess, re
 
 	lib_info = get_lib_info()
 
@@ -43,10 +81,14 @@ def main():
 			help='Library name')
 
 	args = parser.parse_args()
+	libPattern = re.compile(r'^(.+?)((==|!=|>|>=|<|<=)(.*))?$')
 	for lib in args.lib:
-		lib_name = lib.lower()
+		lib_params = libPattern.search(lib).groups()
+		lib_name = lib_params[0].lower()
+		lib_version = lib_params[3]
+		cmp = lib_params[2]
 		if lib_name in lib_info:
-			subprocess.call(['python', lib_info[lib_name], 'install'])
+			install(lib_info[lib_name], lib_version, cmp)
 		else:
 			print(lib + ' is not supported.')
 
--- lib_info/STLport.py
+++ lib_info/STLport.py
@@ -1,44 +1,24 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
 from distutils.dist import Distribution
 
 
-class LibraryDistribution(Distribution):
+def get_dist_list():
+	dist_list = []
 
-	#def parse_config_files(self):
-	#	Distribution.parse_config_files(self)
+	lib_info = {
+		'name': 'STLport',
+		'version': '5.2.1',
+		'author': 'Petr Ovtchenkov',
+		'author_email': '',
+		'description': 'Multiplatform C++ Standard Library (STL implementation). Many compilers and operational environments supported. Standard (ISO/IEC 14882) compliance. Maximum efficiency. Exception and thread safety. Debug mode.',
+		'license': 'Other License',
+		'keywords': '',
+		'url': 'http://stlport.org',
 
-	#def parse_command_line(self):
-	#	return Distribution.parse_command_line(self)
+		'download_url': 'http://sourceforge.net/projects/stlport/files/latest/download',
+	}
+	dist_list.append(Distribution(lib_info))
 
-	def get_command_class(self, command):
-		klass = Distribution.get_command_class(self, command)
-		#print('klass = ')
-		#print(klass)
-		return klass
-
-	def get_command_obj(self, command, create=1):
-		cmd_obj = Distribution.get_command_obj(self, command, create)
-		#print('cmd_obj =')
-		#print(cmd_obj)
-		return cmd_obj
-
-
-setup(
-		name='STLport',
-		version='5.2.1',
-		#scripts=['.py'],
-
-		author='Petr Ovtchenkov',
-		author_email='',
-		description='Multiplatform C++ Standard Library (STL implementation). Many compilers and operational environments supported. Standard (ISO/IEC 14882) compliance. Maximum efficiency. Exception and thread safety. Debug mode.',
-		license='Other License',
-		keywords='',
-		url='http://stlport.org',
-
-		download_url='http://sourceforge.net/projects/stlport/files/latest/download',
-		distclass=LibraryDistribution,
-		)
-
+	return dist_list
