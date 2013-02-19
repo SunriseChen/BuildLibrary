4bfb5a82e0a9b9f474dff269c4e29a6f106e31f1
Sunrise <sunrise0918@gmail.com>
2013-02-19 17:45:01 +0800

    添加项目文件

--- lib_install.py
+++ lib_install.py
@@ -1,18 +1,51 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from ez_setup import use_setuptools
-use_setuptools()
-
 import os, sys, tempfile
+
+EZ_SETUP_URL = 'http://peak.telecommunity.com/dist/ez_setup.py'
+LIB_INFO_DIR = 'lib_info'
+
+
+def download_file(url, target_dir=os.curdir):
+	import urllib2
+
+	print('Downloading ' + url)
+	remote_file = urllib2.urlopen(url)
+
+	if not os.path.exists(target_dir):
+		os.makedirs(target_dir)
+	file_name = os.path.basename(url)
+	file_path = os.path.join(target_dir, file_name)
+	with open(file_path, 'wb') as local_file:
+		local_file.write(remote_file.read())
+
+	remote_file.close()
+	print('Downloaded.')
+
+	return os.path.realpath(file_path)
+
+
+def install_setuptools():
+	print('install setuptools...')
+	download_file(EZ_SETUP_URL)
+	import ez_setup
+	ez_setup.main([])
+	import setuptools
+
+
+try:
+	import setuptools
+except ImportError:
+	install_setuptools()
+
+
 from setuptools.command.easy_install import *
 from setuptools.command.easy_install import rmtree, parse_requirement_arg
 from setuptools.package_index import URL_SCHEME
 from distutils import log
 from distutils.errors import *
 from pkg_resources import *
-
-LIB_INFO_DIR = 'lib_info'
 
 
 class lib_install(easy_install):
@@ -34,7 +67,7 @@ class lib_install(easy_install):
 			self.pth_file.filename = os.path.join(instdir, 'lib-install.pth')
 
 	def easy_install(self, spec, deps=False):
-		self.lib_install(spec, deps)
+		return self.lib_install(spec, deps)
 
 	def lib_install(self, spec, deps):
 		tmpdir = tempfile.mkdtemp(prefix="lib_install-")
@@ -79,6 +112,65 @@ class lib_install(easy_install):
 				rmtree(tmpdir)
 			if dist:
 				self.clean_build_files(dist.project_name)
+
+	def install_item(self, spec, download, tmpdir, deps, install_needed=False):
+
+		# Installation is also needed if file in tmpdir or is not an egg
+		install_needed = install_needed or self.always_copy
+		install_needed = install_needed or os.path.dirname(download) == tmpdir
+		install_needed = install_needed or not download.endswith('.egg')
+		install_needed = install_needed or (
+			self.always_copy_from is not None and
+			os.path.dirname(normalize_path(download)) ==
+			normalize_path(self.always_copy_from)
+		)
+
+		if spec and not install_needed:
+			# at this point, we know it's a local .egg, we just don't know if
+			# it's already installed.
+			for dist in self.local_index[spec.project_name]:
+				if dist.location==download:
+					break
+			else:
+				install_needed = True   # it's not in the local index
+
+		log.info("Processing %s", os.path.basename(download))
+
+		if install_needed:
+			dists = self.install_eggs(spec, download, tmpdir)
+			for dist in dists:
+				self.process_distribution(spec, dist, deps)
+		else:
+			dists = [self.check_conflicts(self.egg_distribution(download))]
+			self.process_distribution(spec, dists[0], deps, "Using")
+
+		if spec is not None:
+			for dist in dists:
+				if dist in spec:
+					return dist
+
+	def run_setup(self, setup_script, setup_base, args):
+		from setuptools.command import bdist_egg, egg_info
+		from setuptools.sandbox import run_setup
+
+		sys.modules.setdefault('distutils.command.bdist_egg', bdist_egg)
+		sys.modules.setdefault('distutils.command.egg_info', egg_info)
+
+		args = list(args)
+		if self.verbose>2:
+			v = 'v' * (self.verbose - 1)
+			args.insert(0,'-'+v)
+		elif self.verbose<2:
+			args.insert(0,'-q')
+		if self.dry_run:
+			args.insert(0,'-n')
+		log.info(
+			"Running %s %s", setup_script[len(setup_base)+1:], ' '.join(args)
+		)
+		try:
+			run_setup(setup_script, args)
+		except SystemExit, v:
+			raise DistutilsError("Setup script exited with %s" % (v.args[0],))
 
 	def generate_setup(self, dist, setup_base):
 		from string import Template
