67584506be6700f51edb19c34fa70c94c674af52
Sunrise Chen <sunrise0918@gmail.com>
2013-04-09 00:25:42 +0800

    添加 boost.python 的嵌入源码编译引用

---
+++ lib_info/Boost/Import/python.cpp
@@ -0,0 +1,37 @@
+// 嵌入源码编译使用 Boost.Python 库
+// 在此直接引入 Boost.Python 库的源文件。
+
+#define BOOST_PYTHON_SOURCE
+
+#include <libs/python/src/numeric.cpp>
+#include <libs/python/src/list.cpp>
+#include <libs/python/src/long.cpp>
+#include <libs/python/src/dict.cpp>
+#include <libs/python/src/tuple.cpp>
+#include <libs/python/src/str.cpp>
+#include <libs/python/src/slice.cpp>
+
+#include <libs/python/src/converter/from_python.cpp>
+#include <libs/python/src/converter/registry.cpp>
+#include <libs/python/src/converter/type_id.cpp>
+#include <libs/python/src/converter/builtin_converters.cpp>
+#include <libs/python/src/converter/arg_to_python_base.cpp>
+
+#include <libs/python/src/object/enum.cpp>
+#include <libs/python/src/object/class.cpp>
+#include <libs/python/src/object/function.cpp>
+#include <libs/python/src/object/inheritance.cpp>
+#include <libs/python/src/object/life_support.cpp>
+#include <libs/python/src/object/pickle_support.cpp>
+#include <libs/python/src/object/iterator.cpp>
+#include <libs/python/src/object/stl_iterator.cpp>
+#include <libs/python/src/object/function_doc_signature.cpp>
+
+#include <libs/python/src/object_protocol.cpp>
+#include <libs/python/src/object_operators.cpp>
+
+#include <libs/python/src/errors.cpp>
+#include <libs/python/src/module.cpp>
+#include <libs/python/src/wrapper.cpp>
+#include <libs/python/src/import.cpp>
+#include <libs/python/src/exec.cpp>
