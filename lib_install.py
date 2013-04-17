d0a136ee1dc67d3753fbb60498b31a3e06877ebe
Sunrise Chen <sunrise0918@gmail.com>
2013-04-18 07:51:14 +0800

    基本实现在 virtualenv 中进行构建

--- lib_install.py
+++ lib_install.py
@@ -34,13 +34,44 @@ def download_file(url, target_dir=os.curdir):
 	return os.path.realpath(file_path)
 
 
-def restart():
-	subprocess.call(['python'] + sys.argv)
-	sys.exit()
+def restart(with_virtualenv=False):
+	if with_virtualenv:
+		if sys.platform == 'win32':
+			batch_file = 'lib_install.bat'
+			commands = [
+				'@echo off',
+				'call %s/Scripts/activate.bat' % PYTHON_ENV,
+				'python %s' % ' '.join(sys.argv),
+				'call %s/Scripts/deactivate.bat' % PYTHON_ENV,
+			]
+		else:
+			batch_file = 'lib_install.sh'
+			commands = [
+				'source %s/bin/activate' % PYTHON_ENV,
+				'python %s' % ' '.join(sys.argv),
+				'%s/bin/deactivate' % PYTHON_ENV,
+			]
+		commands = os.linesep.join(commands)
+
+		try:
+			with open(batch_file, 'w') as f:
+				f.write(commands)
+			subprocess.call([batch_file])
+		finally:
+			if os.path.exists(batch_file):
+				os.remove(batch_file)
+	else:
+		subprocess.call(['python'] + sys.argv)
+
+	sys.exit(0)
+
+
+def check_python():
+	pass
 
 
 def install_setuptools():
-	print('Install setuptools...')
+	print('Installing setuptools ...')
 	setuptools = download_file(SETUPTOOLS_URL)
 	if subprocess.call(['python', setuptools]) == 0:
 		restart()
@@ -59,106 +90,36 @@ def check_setuptools(times=3):
 
 
 def install_virtualenv():
-	print('Install virtualenv...')
-	if subprocess.call(['easy_install', 'virtualenv']) == 0:
-		restart()
-
-
-def virtual_run(commands):
-	suffix = '.bat' if sys.platform == 'win32' else '.sh'
-	fd, path = tempfile.mkstemp(suffix=suffix, dir=os.curdir)
-	try:
-		f = os.fdopen(fd, 'w')
-		if not isinstance(commands, list):
-			commands = [commands]
-
-		if sys.platform == 'win32':
-			commands.insert(0, '@echo off')
-			commands.insert(1, 'call %s/Scripts/activate.bat' % PYTHON_ENV)
-			commands.append('call %s/Scripts/deactivate.bat' % PYTHON_ENV)
-		else:
-			commands.insert(0, 'source %s/bin/activate' % PYTHON_ENV)
-			commands.append('%s/bin/deactivate' % PYTHON_ENV)
-
-		commands = os.linesep.join(commands)
-		f.write(commands)
-		f.close()
-
-		return subprocess.call([path])
-	finally:
-		if os.path.exists(path):
-			os.remove(path)
-
-
-def validate_pair(ob):
-	try:
-		if not (len(ob) == 2):
-			print("Unexpected result:", ob)#, file=sys.stderr)
-			raise ValueError
-	except:
-		return False
-	return True
-
-def consume(iter):
-	try:
-		while True: next(iter)
-	except StopIteration:
-		pass
-
-def get_environment_from_batch_command(env_cmd, initial=None):
-	"""
-	Take a command (either a single command or list of arguments)
-	and return the environment created after running that command.
-	Note that if the command must be a batch file or .cmd file, or the
-	changes to the environment will not be captured.
-
-	If initial is supplied, it is used as the initial environment passed
-	to the child process.
-	"""
-	if not isinstance(env_cmd, (list, tuple)):
-		env_cmd = [env_cmd]
-	# construct the command that will alter the environment
-	env_cmd = subprocess.list2cmdline(env_cmd)
-	# create a tag so we can tell in the output when the proc is done
-	tag = 'Done running command'
-	# construct a cmd.exe command to do accomplish this
-	cmd = 'cmd.exe /s /c "{env_cmd} && echo "{tag}" && set"'.format(**vars())
-	# launch the process
-	proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, env=initial)
-	# parse the output sent to stdout
-	lines = proc.stdout
-	# consume whatever output occurs until the tag is reached
-	consume(itertools.takewhile(lambda l: tag not in l, lines))
-	# define a way to handle each KEY=VALUE line
-	handle_line = lambda l: l.rstrip().split('=',1)
-	# parse key/values into pairs
-	pairs = map(handle_line, lines)
-	# make sure the pairs are valid
-	valid_pairs = filter(validate_pair, pairs)
-	# construct a dictionary of the pairs
-	result = dict(valid_pairs)
-	# let the process finish
-	proc.communicate()
-	return result
+	print('Installing virtualenv ...')
+	return subprocess.call(['easy_install', 'virtualenv']) == 0
 
 
 def check_virtualenv(times=3):
+	if os.getenv('VIRTUAL_ENV') == os.path.abspath(PYTHON_ENV):
+		return
+
 	for i in range(times):
 		try:
 			import virtualenv
-			if not os.path.exists(PYTHON_ENV):
-				subprocess.call(['virtualenv', '--distribute', PYTHON_ENV])
 			break
 		except ImportError:
-			install_virtualenv()
+			if install_virtualenv():
+				break
 	else:
 		print('Install virtualenv fail!')
 		sys.exit(1)
 
+	if not os.path.exists(PYTHON_ENV):
+		subprocess.call(['virtualenv', '--distribute', PYTHON_ENV])
+	sys.argv.insert(1, '--updated')
+	restart(with_virtualenv=True)
+
 
 def install_scons():
-	print('Install SCons...')
-	virtual_run('pip install SCons')
+	print('Installing SCons ...')
+	if subprocess.call(['easy_install', 'SCons']) == 0:
+		sys.argv.insert(1, '--updated')
+		restart()
 
 
 def check_scons(times=3):
@@ -240,11 +201,11 @@ def update_self():
 
 
 def check_env():
+	check_python()
 	check_setuptools()
+	update_self()
 	check_virtualenv()
 	check_scons()
-	sys.exit()
-	update_self()
 
 
 def show_sys_vars():
